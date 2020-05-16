6e635042c17af0706dd759268d26e7ba3ce7a18a
Colby Norman <colbynorm41@gmail.com>
2020-05-16 12:32:00 -0500

    added to project and homework page

--- src/pages/HomeworkPage.js
+++ src/pages/HomeworkPage.js
@@ -6,32 +6,20 @@ function Homework(props) {
         <Container>
         <div>
           <p>{props.title}</p>
-            <a
-              className="nav-link"
-              id="homework"
-              href={props.link}
-            >
+            <a className="nav-link" id="homework" href={props.link}>
               Note Taker
             </a>
-            <a
-              className="nav-link"
-              id="homework"
-              href="https://github.com/Colbynorm/note-taker"
-            >Link To Code
+            <a className="nav-link" id="homework" href="https://github.com/Colbynorm/note-taker">
+              Link To Code
             </a>
         </div>
 
         <div>
           <p>{props.title}</p>
-          <a 
-            className="nav-link"
-            id="homework"
-            href={props.link}
-            >Weather Dashboard</a>
-            <a
-              className="nav-link"
-              id="homework"
-              href="https://colbynorm.github.io/weather-dashboard/"
+          <a className="nav-link" id="homework" href={props.link}>
+            Weather Dashboard
+            </a>
+            <a className="nav-link" id="homework" href="https://colbynorm.github.io/weather-dashboard/"
             >Live Link To App
             </a>
         </div>
--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -7,33 +7,20 @@ function Project(props){
       <Container>
         <div>
           <p>{props.title}</p>
-            <a
-              className="nav-link"
-              id="project"
-              href={props.link}
-            >
+            <a className="nav-link" id="project" href={props.link}>
               Quarantine Shopping App
             </a>
-            <a
-              className="nav-link"
-              id="project"
-              href="https://doomsday-webstore.herokuapp.com/"
-            >Live Link
+            <a className="nav-link" id="project" href="https://doomsday-webstore.herokuapp.com/">
+              Live Link
             </a>
         </div>
 
         <div>
           <p>{props.title}</p>
-          <a 
-            className="nav-link"
-            id="project"
-            href={props.link}
-            >Classroom Self Check In</a>
-            <a
-              className="nav-link"
-              id="project"
-              href="https://kayla8693.github.io/classroom-self-checkin"
-            >Live Link
+          <a className="nav-link" id="project" href={props.link}>
+            Classroom Self Check In</a>
+            <a className="nav-link" id="project" href="https://kayla8693.github.io/classroom-self-checkin">
+              Live Link
             </a>
         </div>
       </Container>
